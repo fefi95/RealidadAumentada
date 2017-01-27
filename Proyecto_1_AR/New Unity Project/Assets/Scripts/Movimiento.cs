28bedf8ab18fc63de264e506caf9e4870213b5a0
Sergio <alenios@hormail.com>
2017-01-27 06:15:10 -0400

    overhaul nivel de agua

--- Proyecto_1_AR/New Unity Project/Assets/Scripts/Movimiento.cs
+++ Proyecto_1_AR/New Unity Project/Assets/Scripts/Movimiento.cs
@@ -23,7 +23,7 @@ public class Movimiento : MonoBehaviour {
                 z = (camara.transform.eulerAngles.x)/(45);
             }
             else if ((camara.transform.eulerAngles.x > 315)&&(camara.transform.eulerAngles.x <= 360)){
-                z = -(camara.transform.eulerAngles.x - 315)/(45);
+                z = (camara.transform.eulerAngles.x - 360)/(45);
             }
         }
         else if ((camara.transform.eulerAngles.z > 45)&&(camara.transform.eulerAngles.z <= 135)){
@@ -33,19 +33,21 @@ public class Movimiento : MonoBehaviour {
                 z = (camara.transform.eulerAngles.y)/(45);
             }
             else if ((camara.transform.eulerAngles.y > 315)&&(camara.transform.eulerAngles.y <= 360)){
-                z = -(camara.transform.eulerAngles.y - 315)/(45);
+                z = (camara.transform.eulerAngles.y - 360)/(45);
+
             }
         }
         else if ((camara.transform.eulerAngles.z > 135)&&(camara.transform.eulerAngles.z <= 225)){
             x = -(camara.transform.eulerAngles.z - 180)/(45);
+            Debug.Log(camara.transform.eulerAngles.x);
             y = (1 - x);
             if ((camara.transform.eulerAngles.x > 0)&&(camara.transform.eulerAngles.x <= 45)){
                 z = -(camara.transform.eulerAngles.x)/(45);
             }
             else if ((camara.transform.eulerAngles.x > 315)&&(camara.transform.eulerAngles.x <= 360)){
-                z = (camara.transform.eulerAngles.x - 315)/(45);
+                z = -(camara.transform.eulerAngles.x - 360)/(45);
             }
-
+            Debug.Log(z);
         }
         else if ((camara.transform.eulerAngles.z > 225)&&(camara.transform.eulerAngles.z <= 315)){
             y = -(camara.transform.eulerAngles.z - 270)/(45);
@@ -54,11 +56,11 @@ public class Movimiento : MonoBehaviour {
                 z = -(camara.transform.eulerAngles.y)/(45);
             }
             else if ((camara.transform.eulerAngles.y > 315)&&(camara.transform.eulerAngles.y <= 360)){
-                z = (camara.transform.eulerAngles.y - 315)/(45);
+                z = -(camara.transform.eulerAngles.y - 360)/(45);
             }
         }
         else if ((camara.transform.eulerAngles.z > 315)&&(camara.transform.eulerAngles.z <= 360)){
-            x = -(camara.transform.eulerAngles.z - 315)/(45);
+            x = (camara.transform.eulerAngles.z - 360)/(45);
             y = -(1 - x);
             if ((camara.transform.eulerAngles.x > 0)&&(camara.transform.eulerAngles.x <= 45)){
                 z = (camara.transform.eulerAngles.x)/(45);
@@ -68,13 +70,17 @@ public class Movimiento : MonoBehaviour {
             }
 
         }
+        ReturnInBound();
 
 
-
-        Debug.Log(camara.transform.eulerAngles.z);
         //vectorMov = new Vector3(x,0,z);
         //RigidBodyPelota.velocity = vectorMov;
         //Physics.gravity = new Vector3(camara.transform.eulerAngles.z,camara.transform.eulerAngles.y,-camara.transform.eulerAngles.x);
         Physics.gravity = new Vector3(x,y,(-z));
+    }
+    void ReturnInBound(){
+        if ((transform.position.x < -0.2)||(transform.position.x > 0.2)||(transform.position.y < -0.2)||(transform.position.y > 0.2)||(transform.position.z < -0.2)||(transform.position.z > 0.2)){
+            transform.position = new Vector3(0,0,0);
+        }
     }
 }
