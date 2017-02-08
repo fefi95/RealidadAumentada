f82f7a273ae8f2c5ec7a8ea44aa9cde0a1db3b2f
Stefani Castellanos <Stefani Castellanos>
2017-02-07 21:27:15 -0400

    Empezando cuarto con iman

---
+++ Proyecto_1_AR/New Unity Project/Assets/Scripts/ControladorIman.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorIman : MonoBehaviour {
+
+    public LayerMask m_MagneticLayers;
+    public Vector3 m_Position;
+    public float m_Radius;
+    public float m_Force;
+
+    void FixedUpdate()
+    {
+        Collider[] colliders;
+        Rigidbody rigidbody;
+
+        colliders = Physics.OverlapSphere(transform.position + m_Position, m_Radius, m_MagneticLayers);
+        print(colliders.Length);
+        foreach (Collider collider in colliders)
+        {
+            print("holis!");
+            rigidbody = (Rigidbody)collider.gameObject.GetComponent(typeof(Rigidbody));
+            if (rigidbody != null)
+            {
+                print("collide:" + rigidbody.name);
+                rigidbody.AddExplosionForce(m_Force * -1, transform.position + m_Position, m_Radius);
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected() {
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position + m_Position, m_Radius);
+    }
+}
